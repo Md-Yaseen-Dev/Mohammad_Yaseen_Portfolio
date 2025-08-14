9040dda79974479b73e20ce5f2edcd10eab3e49b
Mohammad Yaseen <ysn6040@gmail.com>
2025-08-14 14:32:37 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
   build: {
@@ -25,7 +23,6 @@ export default defineConfig({
   server: {
     fs: {
       strict: true,
-      deny: ["**/.*"],
     },
   },
 });
